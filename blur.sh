53018dc61f03189c47c2661488b1f5e5889b66eb
PRATAP <pratap@fastmail.fm>
2020-05-05 17:40:47 +0530

    Add files via upload

---
+++ blur.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+source="/usr/share/gnome-shell/theme/Yaru/gnome-shell-theme.gresource"
+prefix="/org/gnome/shell/theme"
+dest="/usr/local/share/gnome-shell/theme/blur"
+
+install -D /dev/null $dest/gdm3.css
+install -D /dev/null $dest/blur.gresource.xml
+install -d $dest/icons/scalable/actions
+
+bg=$(sudo -Hu admin dbus-launch gsettings get org.gnome.desktop.background picture-uri)
+file=$(echo $bg | cut -c9- | rev | cut -c2- | rev)
+
+cp $file $dest
+
+img=$(basename -a $file)
+
+cd $dest
+convert $img -channel RGBA -blur 0x26 blur.png
+
+gresource extract $source $prefix/gdm3.css > $dest/original.css
+gresource extract $source $prefix/checkbox.svg > $dest/checkbox.svg
+gresource extract $source $prefix/checkbox-off.svg > $dest/checkbox-off.svg
+gresource extract $source $prefix/checkbox-focused.svg > $dest/checkbox-focused.svg
+gresource extract $source $prefix/checkbox-off-focused.svg > $dest/checkbox-off-focused.svg
+gresource extract $source $prefix/toggle-on.svg > $dest/toggle-on.svg
+gresource extract $source $prefix/toggle-off.svg > $dest/toggle-off.svg
+gresource extract $source $prefix/icons/scalable/actions/pointer-drag-symbolic.svg > $dest/icons/scalable/actions/pointer-drag-symbolic.svg
+gresource extract $source $prefix/icons/scalable/actions/keyboard-enter-symbolic.svg > $dest/icons/scalable/actions/keyboard-enter-symbolic.svg
+gresource extract $source $prefix/icons/scalable/actions/keyboard-hide-symbolic.svg > $dest/icons/scalable/actions/keyboard-hide-symbolic.svg
+gresource extract $source $prefix/icons/scalable/actions/pointer-secondary-click-symbolic.svg > $dest/icons/scalable/actions/pointer-secondary-click-symbolic.svg
+gresource extract $source $prefix/icons/scalable/actions/keyboard-shift-filled-symbolic.svg > $dest/icons/scalable/actions/keyboard-shift-filled-symbolic.svg
+gresource extract $source $prefix/icons/scalable/actions/keyboard-caps-lock-filled-symbolic.svg > $dest/icons/scalable/actions/keyboard-caps-lock-filled-symbolic.svg
+gresource extract $source $prefix/icons/scalable/actions/pointer-primary-click-symbolic.svg > $dest/icons/scalable/actions/pointer-primary-click-symbolic.svg
+gresource extract $source $prefix/icons/scalable/actions/keyboard-layout-filled-symbolic.svg > $dest/icons/scalable/actions/keyboard-layout-filled-symbolic.svg
+gresource extract $source $prefix/icons/scalable/actions/eye-not-looking-symbolic.svg > $dest/icons/scalable/actions/eye-not-looking-symbolic.svg
+gresource extract $source $prefix/icons/scalable/actions/pointer-double-click-symbolic.svg > $dest/icons/scalable/actions/pointer-double-click-symbolic.svg
+gresource extract $source $prefix/icons/scalable/actions/eye-open-negative-filled-symbolic.svg > $dest/icons/scalable/actions/eye-open-negative-filled-symbolic.svg
+
+echo '@import url("resource:///org/gnome/shell/theme/original.css");
+  #lockDialogGroup {
+  background: '#042320' url('file://$dest/../blur.png');
+  background-repeat: no-repeat;
+  background-size: cover;;
+  background-position: center; }' > $dest/gdm3.css
+
+echo '<?xml version="1.0" encoding="UTF-8"?>
+<gresources>
+  <gresource prefix="/org/gnome/shell/theme">
+    <file>original.css</file>
+    <file>gdm3.css</file>
+    <file>toggle-off.svg</file>
+    <file>checkbox-off.svg</file>
+    <file>toggle-on.svg</file>
+    <file>checkbox-off-focused.svg</file>
+    <file>checkbox-focused.svg</file>
+    <file>checkbox.svg</file>
+    <file>icons/scalable/actions/pointer-drag-symbolic.svg</file>
+    <file>icons/scalable/actions/keyboard-enter-symbolic.svg</file>
+    <file>icons/scalable/actions/keyboard-hide-symbolic.svg</file>
+    <file>icons/scalable/actions/pointer-secondary-click-symbolic.svg</file>
+    <file>icons/scalable/actions/keyboard-shift-filled-symbolic.svg</file>
+    <file>icons/scalable/actions/keyboard-caps-lock-filled-symbolic.svg</file>
+    <file>icons/scalable/actions/pointer-primary-click-symbolic.svg</file>
+    <file>icons/scalable/actions/keyboard-layout-filled-symbolic.svg</file>
+    <file>icons/scalable/actions/eye-not-looking-symbolic.svg</file>
+    <file>icons/scalable/actions/pointer-double-click-symbolic.svg</file>
+    <file>icons/scalable/actions/eye-open-negative-filled-symbolic.svg</file>
+  </gresource>
+</gresources>' > $dest/blur.gresource.xml
+
+glib-compile-resources blur.gresource.xml
+mv blur.gresource ..
+mv blur.png ..
+rm -r $dest
+
+update-alternatives --quiet --install /usr/share/gnome-shell/gdm3-theme.gresource gdm3-theme.gresource /usr/local/share/gnome-shell/theme/blur.gresource 0
+update-alternatives --quiet --set gdm3-theme.gresource /usr/local/share/gnome-shell/theme/blur.gresource
+
+
+
+
